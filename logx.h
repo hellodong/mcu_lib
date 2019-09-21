43605f22688e726e2485162c65a569c178a579de
purefarmer <purefarmer@protonmail.com>
2019-09-21 01:43:00 +0100

    test(): 添加log输出信息

--- logx.h
+++ logx.h
@@ -9,8 +9,21 @@
 extern "C" {
 #endif
 
-  
-#define LOG(fmt, ...)	do{pri_printf("%s() #%d "fmt, __FUNCTION__,__LINE__, ##__VA_ARGS__);}while(0)
+
+#define __ERROR		0
+#define __WARN		1
+#define __INFO		2
+#define __DBG		3
+#define LEVEL_LOG	(__DBG)
+
+#define log_print(fmt,...)	pri_printf(fmt, ##__VA_ARGS__)
+#define LOG_DBG(fmt,...)	do{if(LEVEL_LOG >= __DBG)	log_print("[DBG] %s() #%d "fmt"\n", __func__, __LINE__, ##__VA_ARGS__);}while(0)
+#define LOG_INF(fmt,...)	do{if(LEVEL_LOG >= __INFO)	log_print("[INF] %s() #%d "fmt"\n", __func__, __LINE__, ##__VA_ARGS__);}while(0)
+#define LOG_WRN(fmt,...)	do{if(LEVEL_LOG >= __WARN)	log_print("[WRN] %s() #%d "fmt"\n", __func__, __LINE__, ##__VA_ARGS__);}while(0)
+#define LOG_ERR(fmt,...)	do{if(LEVEL_LOG >= __ERROR)	log_print("[ERR] %s() #%d "fmt"\n", __func__, __LINE__, ##__VA_ARGS__);}while(0)
+#define LOG(fmt,...)		LOG_DBG(fmt,##__VA_ARGS__)
+
+
 	
 
 #ifdef __cplusplus
